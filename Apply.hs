77c0e4a31e94d96c5839512a1f4c3679a1a1aa81
Evan Rinehart <erinehart@searchinfluence.com>
2013-01-09 19:09:41 -0600

    case apply and pattern match code

---
+++ Apply.hs
@@ -0,0 +1,34 @@
+module Apply where
+
+import Data.Map
+import qualified Data.Map as M
+import Data.Maybe
+import Control.Monad
+
+import Expr
+import Value
+import Case
+
+apply :: (Case Expr) -> Value -> Maybe (Env, Expr)
+apply (Case []) _ = Nothing
+apply (Case ((p,expr):cases)) v = case match p v of
+  Nothing -> apply (Case cases) v
+  Just env -> Just (env, expr)
+
+match :: Pattern -> Value -> Maybe Env
+match p v = case p of
+  PatVar s -> Just (M.singleton s v)
+  PatNum n -> case v of
+    Number n' -> if n' == n then Just M.empty else Nothing
+    _ -> Nothing
+  PatSym y -> case v of
+    Symbol y' -> if y' == y then Just M.empty else Nothing
+    _ -> Nothing
+  PatDontCare -> Just M.empty
+  PatTuple pats -> case v of
+    Tuple vs -> if length vs /= length pats
+      then Nothing
+      else do
+        envs <- zipWithM match pats vs
+        Just (unions envs)
+    _ -> Nothing
