2621ee7cd55dd320067065e331a606cda3d47a59
EvanR <evanrinehart@gmail.com>
2013-01-12 20:04:13 -0600

    fixed main object

--- Main.hs
+++ Main.hs
@@ -3,19 +3,35 @@ module Main where
 import qualified Data.Map as M
 import Control.Concurrent
 import qualified Data.ByteString as BS
+import System.Exit
 
 import Value
 import Global
 import Object
-import Repl
+import Parser
+import Eval
 
 main :: IO ()
 main = do
   g <- newEmptyGlobal
+  src <- BS.getContents
+  prog <- case parse src of
+    Left err -> do
+      putStrLn err
+      exitFailure
+    Right prog -> return prog
+
   installGlobalObject g (Symbol "stdout") $ \obj arg -> do
     putStrLn ("STDOUT: " ++ (showValue arg))
     return (ObjectNormal, Symbol "null")
-  src <- BS.getContents
-  rep g src
+
+  installGlobalObject g (Symbol "main") $ \obj _ -> do
+    ans <- eval g obj emptyEnv prog
+    return (ObjectNormal, ans)
+
+  port <- newEmptyMVar
+  ans <- systemRequest g (Symbol "main") (Symbol "run") port
+  case ans of
+    Left err -> putStrLn ("MainMain: ERROR "++err)
+    Right v -> putStrLn ("MainMain: exit value "++showValue v)
   threadDelay 1000000
-  return ()
--- Repl.hs
+++ Repl.hs
@@ -9,16 +9,3 @@ import Value
 import Object
 import Eval
 
--- one shot
-rep :: Global -> ByteString -> IO ()
-rep g src = case parse src of
-  Left err -> do
-    Prelude.putStrLn err
-  Right expr -> do
-    installGlobalObject g (Symbol "main") $ \obj _ -> do
-      ans <- eval g obj emptyEnv expr
-      return (ObjectHalt, ans)
-    port <- newEmptyMVar
-    ans <- systemRequest g (Symbol "main") (Symbol "run") port
-    Prelude.putStrLn (show ans)
-    return ()
